67c1f9d318acd6e1d71e7a7bedd2fb031911c2d6
itshmnt <hemantxs5@gmail.com>
2025-05-18 15:57:15 +0530

    adding header

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
+    <div className="pt-16">
       <h1>SplitKar App</h1>
-    </>
+    </div>
   );
 }
